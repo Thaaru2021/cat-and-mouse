633bec1bff97f49851709205f0d26e6ba706260c
Thaaru2021 <77873537+Thaaru2021@users.noreply.github.com>
2021-02-12 11:37:40 +0530

    Add files via upload

---
+++ project 20/tomAndJerryTemplate-master/sketch.js
@@ -0,0 +1,29 @@
+
+function preload() {
+ tom.loadAnimation ("cat1.png,cat2.png,cat3.png,cat4.png") 
+ garden.loadImage("garden.png")
+ jerry.loadAnimation("mouse1.png,mouse2.png,mouse3.png,mouse4.png")
+
+}
+
+function setup(){
+    createCanvas(1000,800);
+  tom
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
